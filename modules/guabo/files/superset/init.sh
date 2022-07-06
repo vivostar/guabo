e5b72426cf38ea4b6059409e58b525c77b9514ff
Peng Lee <lishuaipeng651@gmail.com>
2022-07-07 01:01:42 +0800

    add superset dev

---
+++ modules/guabo/files/superset/init.sh
@@ -0,0 +1,4 @@
+yum install -y git yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+systemctl start docker
